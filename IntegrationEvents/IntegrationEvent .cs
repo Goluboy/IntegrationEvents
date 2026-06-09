feee78616daf9e53bd2c2afe81beec3e2fae5c6b
golubaya <35600056+Goluboy@users.noreply.github.com>
2026-06-09 23:51:18 +0500

    update EventId

--- IntegrationEvents/IntegrationEvent .cs
+++ IntegrationEvents/IntegrationEvent .cs
@@ -1,8 +1,20 @@
-﻿namespace IntegrationEvents.IntegrationEvents;
+﻿using MassTransit;
+
+namespace IntegrationEvents.IntegrationEvents;
 
 public abstract record IntegrationEvent
 {
-    public Guid EventId { get; init; } = Guid.NewGuid();
+    public Guid EventId { get; init; }
     public DateTime OccurredOn { get; init; } = DateTime.UtcNow;
     public Guid CorrelationId { get; init; } // = OrderId для связки шагов SAGA
+
+    protected IntegrationEvent()
+    {
+        EventId = NewId.NextSequentialGuid();
+        OccurredOn = DateTime.UtcNow;
+    }
+    protected IntegrationEvent(Guid correlationId) : this()
+    {
+        CorrelationId = correlationId;
+    }
 }
